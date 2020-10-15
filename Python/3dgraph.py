d0ec100f4aeec098bc9d75ea92d0a8195a2e31cc
Mehran Ali Ansari <34086637+emehrawn@users.noreply.github.com>
2020-10-15 19:41:45 +0530

    Delete 3dgraph.py

--- Python/3dgraph.py
+++
@@ -1,8 +0,0 @@
-#this is a basic 3d graph
-from mpl_toolkits.mplot3d import axes3d
-import matplotlib
-fig= plt.figure()
-graph=fig.add_subplot(1,1,1, projection = '3d')
-X,Y,Z= [1,2,3,4,5,6,7], [2,3,4,5,8,10,14], [3,8,9,7,10,8,11]
-graph.plot(X,Y,Z)                            #graph.plot_wireframe(X,Y,Z) is the old command
-print(matplotlib.pyplot.show())
