b0b8d37f6cb496732f1ead33a27ee48c8a5940b8
Mehran Ali Ansari <34086637+emehrawn@users.noreply.github.com>
2020-10-15 19:42:06 +0530

    Delete 2pl2dict.py

--- Python/2pl2dict.py
+++
@@ -1,9 +0,0 @@
-#convert the data in tuples and list into dictionary
-
-
-daata=[("Mehran", 4) , ("Ali", 7) , ("Abbas", 1) , ("Hussain", 5) , ("Muhammad", 56)]
-trick = {}
-for value, value2 in daata:
-    trick[value]=value2
-
-print(trick)    
